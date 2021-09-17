473da1214a7200a3e38af0ee463635dbd479ff01
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2021-09-17 11:21:00 +0700

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ $.ajax({
         for (a = 0; a < response.Surah.length; a++) {
             // console.log(response.Surah[a].name);
             $('.name-surah').append(`
-                <option value="${response.Surah[a].number}">${response.Surah[a].number}. ${response.Surah[a].name}</option>
+                <option value="${response.Surah[a].number}">${response.Surah[a].number}. ${response.Surah[a].name} - (${response.Surah[a].numberOfAyahs} ayat)</option>
             `);
 
         }
@@ -26,10 +26,12 @@ $("#set").click(() => {
         beforeSend: function() {
             $("#loading").show();
             $('.alquran').hide();
+            $('.judul').hide()
         },
         complete: function() {
             $("#loading").hide();
             $('.alquran').show();
+            $('.judul').show()
         },
         success: ((response) => {
             // console.log(response.Surah.ayahs.length);
@@ -38,6 +40,8 @@ $("#set").click(() => {
             $('.saatini').html('');
             $('.judul-title').html('');
             $('.artinya').html('')
+            $('.detail-quran').html('')
+            $('.modal-quran-all').html('')
             $('body').append(`
                 <div class="saatini">Anda Membaca Surah: <strong>${response.Surah.name}</strong></div>
                 `)
@@ -58,17 +62,52 @@ $("#set").click(() => {
             }
 
             $('.judul').append(`
-                <h2 class="text-center judul-title"><strong>${response.Surah.name}</strong></h2>
+                <h2 class="text-center judul-title"><strong>${response.Surah.name}</strong> <strong class="ayat">${response.Surah.asma}</strong></h2>
                 <div class="text-center artinya">${response.Surah.translationId}</div>
+
             `)
             for (a = 0; a < response.Surah.ayahs.length; a++) {
 
                 $('.alquran').append(`
-                <div class="shadow">
-                    <div class="ayat">${response.Surah.ayahs[a].ayahText} <div class="ayat-ke">${response.Surah.ayahs[a].verseId}</div></div>
-                    <div class="arti">${response.Surah.ayahs[a].indoText}</div>
-                </div>
-            `)
+                    <div class="shadow">
+                        <div class="ayat">${response.Surah.ayahs[a].ayahText} <div class="ayat-ke">${response.Surah.ayahs[a].verseId}</div></div>
+                        <div class="arti">${response.Surah.ayahs[a].indoText}</div>
+                        <button class="btn btn-sm btn-link" data-toggle="modal" data-target="#exampleModal-${response.Surah.ayahs[a].verseId}">Detail</button>
+                    </div>
+                `)
+
+                var id_ = `exampleModal-${response.Surah.ayahs[a].verseId}`
+                    // $(".modal-quran").attr('id', id_)
+                var test = `
+                        <div class="modal fade modal-quran" id="${id_}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        Surat ${response.Surah.name} Ayat ke ${response.Surah.ayahs[a].verseId}
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                    </div>
+                                    <div class="modal-body detail-quran">
+                                        <div class="ayat text-right">
+                                            ${response.Surah.ayahs[a].ayahText} 
+                                        </div>
+                                        <audio controls>
+                                            <source src="${response.Surah.ayahs[a].audio}" type="audio/mp3">
+                                        </audio>
+                                        <div class="indo"><i>${response.Surah.ayahs[a].readText}</i></div>
+                                        <br>
+                                        <div class="indo">${response.Surah.ayahs[a].indoText}</div>
+                                        <hr>
+                                        <div class="turun">Diturunkan di <strong>${response.Surah.typeId}</strong></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `
+
+                $('.modal-quran-all').append(test)
+
 
             }
         })
